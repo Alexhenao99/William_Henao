2b19be1d24b34669160d29ea7cd69a3908b2fbdb
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-05-10 01:03:02 -0500

    Add dark mode

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className='dark:bg-blue'>
+      <h1 className='text-center w-full p-10  text-9xl font-bold text-blue dark:text-white'>
+        Este sera el home
+      </h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/DarkMode.js
@@ -0,0 +1,60 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import luna from '../images/iconos/luna.png'
+import sol from '../images/iconos/sol.png'
+import Image from 'next/image'
+
+const DarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  useEffect(() => {
+    const localDark = JSON.parse(localStorage.getItem('isDarkMode')) || false
+    setIsDarkMode(localDark)
+    if (localDark) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [])
+
+  const handleToggle = () => {
+    const newIsDarkMode = !isDarkMode
+    setIsDarkMode(newIsDarkMode)
+    if (!newIsDarkMode) {
+      document.documentElement.classList.remove('dark')
+      localStorage.setItem('isDarkMode', 'false')
+    } else {
+      document.documentElement.classList.add('dark')
+      localStorage.setItem('isDarkMode', 'true')
+    }
+  }
+
+  return (
+    <button
+      className='absolute p-2 z-50'
+      onClick={handleToggle}
+    >
+      {isDarkMode
+        ? (
+          <Image
+            src={luna}
+            width={50}
+            height={50}
+            alt='Luna'
+          />
+          )
+        : (
+          <Image
+            src={sol}
+            width={50}
+            height={50}
+            alt='sol'
+          />
+          )}
+    </button>
+  )
+}
+
+export default DarkMode
