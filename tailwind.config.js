dbede33e1cd003d79b340526c76344188df2b401
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-05-11 01:03:55 -0500

    Add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   darkMode: 'class',
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     colors: {
       text: '#0A1929',
@@ -12,7 +8,9 @@ module.exports = {
       text_dark: '#ffffff',
       bg: '#ffffff',
       bg_dark: '#0A1929',
-      bg_dark_secondary: '#356C78'
+      bg_dark_secondary: '#356C78',
+      circle: 'rgba(10, 25, 41, 0.95)',
+      circle_dark: 'rgba(53, 108, 120, 0.95)'
     }
   },
   plugins: []
