2cda8a29d9ebb09195804771df36f34eb705efd3
Alexanderhenao99 <alexanderhenao0522@gmail.com>
2023-05-15 15:59:23 -0500

    Add button if cannot see the cv

--- src/components/Cv.js
+++ src/components/Cv.js
@@ -1,6 +1,17 @@
+import { cvUrl } from '@/utils/personalData'
+
 const Cv = () => {
   return (
-    <div className='pt-14 h-full rounded-bl-3xl rounded-br-3xl'>
+    <div className='relative pt-14 h-full rounded-bl-3xl rounded-br-3xl'>
+      <a
+        href={cvUrl()}
+        target='_blank'
+        rel='noopener noreferrer'
+        className='absolute top-3 right-20 text-text_dark dark:text-text bg-text dark:bg-bg_dark_secondary px-4 py-1 rounded-2xl hover:scale-105 hover:drop-shadow-light_shadow dark:hover:drop-shadow-dark_shadow'
+      >
+        Ver desde Google Drive.
+      </a>
+
       <embed
         src='/CV_2023.pdf'
         className='rounded-bl-3xl rounded-br-3xl'
