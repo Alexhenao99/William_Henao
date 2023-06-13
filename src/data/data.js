7b9ba71a816737bdd64d3d269cbec707b3bb2d29
Alex Henao <alexanderhenao0522@gmail.com>
2023-06-13 12:31:20 -0500

    Update data

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,45 @@
-import perfil from '../images/photo CV.png'
+//* images FusionaJobs
+// imgF https://lh3.googleusercontent.com/drive-viewer/AFGJ81qChySPHCIvgIRQCCLGpZKueFJASoHIjvfLbgXOCciGaO-uQGmFEkOYnYNTBFOWx58N9WeTnbYMow1adRfpTkNm1mZBSQ=w1236-h980
+// imgF2 https://lh3.googleusercontent.com/drive-viewer/AFGJ81pDy3BuxbA6x81wgbitLPnkdqy_BQc5MZqo49Ekt-bAak2HN7nJQ_q_YInRzQmqOo1BED4imwZ0QoFnSruRDLIGHlBw0Q=w1236-h980
+// imgF2b https://lh3.googleusercontent.com/drive-viewer/AFGJ81ruSFnN3vGWrFSOD7NYW00XoNRwpsPgVVZs-jrmftiXQls05xq401bqmx9xFRwo-RDSvalhkpceQfu6sfLpcnPBRmIUQA=w1236-h980
+// imgF3 https://lh3.googleusercontent.com/drive-viewer/AFGJ81rTuDlJBuJ41hTyva-a2ypqc-VYplCreGgjuBL9goUqQOd_wL3KYpEdAbWiyXyiQfE6rwCSmVPQn9CxBJCXKXMEv4n3Ig=w1236-h980
+// imgF4 https://lh3.googleusercontent.com/drive-viewer/AFGJ81oq5WOs2FzxGQwIQWeKN4Jvyd8ngJ0h4d68MtyPJHq-njyT_CDPsnKBv17vj38B6QgLBm4XiTp7Pq0EZcS-D-fMVxCTuA=w1236-h980
+// imgF5 https://lh3.googleusercontent.com/drive-viewer/AFGJ81o8Qo_oUNQF79BvAr-FlHWNOSP5PveuKry26_bEFD3E_x1Ivf8RvgWvwGMFY8Qd0g2os96Tc_qlvw9sTHztixyEZuYcMg=w1236-h980
+// imgF6 https://lh3.googleusercontent.com/drive-viewer/AFGJ81rA5wpvffD9qTYouruBQq-15yS50Kz4SdMBvDDb7oQw9fa6uIgBG1539_kh7Up6TdiNAqDP7kzRi0IMrOze_ypK6D8BpQ=w1236-h980
+// imgF7 https://lh3.googleusercontent.com/drive-viewer/AFGJ81obDWl8GFjT8IAw4LMCkyxOX-wtKj3qVhvSJ7y7csn2aombyIye8tHH0rSr2TQBiKCTRo3hOjwVK3jMCsTE3HZ3Y6tX=w1236-h980
+// imgF8 https://lh3.googleusercontent.com/drive-viewer/AFGJ81o0FcQ-HaZOZPYoDBVYleJbLDGKs_SuytiCF-abw_fY3VryohcjL4CTzuFIOYCBN8Qbdb26ZwFPRu31PpANVtrfB0vA=w1236-h980
+// imgF9 https://lh3.googleusercontent.com/drive-viewer/AFGJ81r5Kf8N0RU5Dt_k06-NbRVitwPINze7ncue9Ln8a_OoirFBgn1H_0ff44OrvyJoWnGaY7ayJFLBoxfZ-EPVdy5jHSqV=w1236-h980
+// imgF10 https://lh3.googleusercontent.com/drive-viewer/AFGJ81p3L45KUU30RABtF-zsfKk0vUK6_PKVOJLP-ZvZBw938k37nFyzMozhyEohKl7s07UeMov8dd-vYW1UQNv16OvBZ1qa1g=w1236-h980
+// imgF11 https://lh3.googleusercontent.com/drive-viewer/AFGJ81pGZq17WIRvr-RtXZmqKSCVGR_y3YYoSVzSTHd9_rmBYfyIcDjPWqlZw49ZrPENjdvI1M_2R9MllWK0q9--77MYw1t3Vw=w1236-h980
+// imgF12 https://lh3.googleusercontent.com/drive-viewer/AFGJ81oGGNkadt97gswuFpt6AdqTFkaP7hTmmCV52rSjKWudHOHi_M3IBNW7U5tBlDiHdcJ6NHDjJQBZhgmd772ArFpV_Kw1Yw=w1236-h980
+// imgF13 https://lh3.googleusercontent.com/drive-viewer/AFGJ81obhNQb2SlurIGK2kATGo9WDG51_G_2niSU7fvYuMp8xzp72QhVAgOXUR64R-4JRLGgnSNxkkDCpwZmYASXjE3aVlYJ=w1236-h980
+// imgF14 https://lh3.googleusercontent.com/drive-viewer/AFGJ81oq-CFRphOetefZX74xBszUZecjC15ynXqPwf9E3x1UGtgjgnT_FfWTS8pQT3bVEg7bgAswVKJ20RExtHXF_UOLluBk0A=w1236-h980
+// imgF15 https://lh3.googleusercontent.com/drive-viewer/AFGJ81rzB99E1cjA__oLOWI79q7OalFIBuV721i0sypTGIPchQ8NwgKOOrIxZc3lQH0oBSiFJjzj4xjjhFSBcmhub6_eToCp=w1236-h980
+// imgF16 https://lh3.googleusercontent.com/drive-viewer/AFGJ81qUmVDyYaFHx43bfboupnBZvgm1qMvReMWh3f4n3FWsF99VXm08KUa08EPlPkll809gE0VoWorGcCKARFats1WrWT0e2w=w1236-h980
+// imgF17 https://lh3.googleusercontent.com/drive-viewer/AFGJ81pI7cG6mFqOwvJhUal91WubnPcHloWw28Ia4aQ_-N5WmDv20SguAgdcvpuDaaoSUBZVUe4joHrP3irpJIqrstmQr0ZQoQ=w1236-h980
+// imgF18 https://lh3.googleusercontent.com/drive-viewer/AFGJ81obpZWZgF4VREPbUhwfLwVmmOnqNCLmEb8-Yu3i9sX-IPZIgBmW6LI0EAA_vOZiBsu5ijYRuvgKMv10ba1ETeh5UnB0=w1236-h980
+// imgF19 https://lh3.googleusercontent.com/drive-viewer/AFGJ81r_OV_6Px7RyDnKxGDg-Tyn5z5lgpfITLfmucJPy4Yz5A6wWt8qKXAyDz8_pNuk-2n8d8dSgEUtJlU14L2BVACIu2XRhg=w1236-h980
+// imgF20 https://lh3.googleusercontent.com/drive-viewer/AFGJ81qPxeG5whmADP-_Q8GygIwoKcOS_BguWyW_Krs_pJem8teBIL4PZiKQEPhTqO0zfYTDCKBkrFfeeDFaylpRuR8D-7sg=w1236-h980
+// imgF21 https://lh3.googleusercontent.com/drive-viewer/AFGJ81okUZo-LNePmqsvsTQPT6ganSETfG37bLFqDM7---PfCp4Vz51b3lo-Y0Cz3wbO40_UY850KvcdiSR3XK0-IukYhzWf=w1236-h980
+// imgF22 https://lh3.googleusercontent.com/drive-viewer/AFGJ81pJcUI3xCaGAie8RJiHxCltC0xVvijXzdEAj1QJXo_Dl584YYBaE5P7nTN9jcx1pQlwbO4F9aanJpDedel99Ee33QBMGA=w1236-h980
+// imgF23 https://lh3.googleusercontent.com/drive-viewer/AFGJ81ruR1KTgG2zCyYjTYPHXbOlP4YtPYhWLGi6YhLcsFoUKJpgegu3p4Jorsf61aieiD30h-FoM6Jxk9qWKyulTGzW9o5TPg=w1236-h980
+// imgF24 https://lh3.googleusercontent.com/drive-viewer/AFGJ81qrTrdpsv3XUuNJ8tZVRJYYF-lSkv3UV8PDMd6hbhjuZmfe4b-G1l1iw9yKh6cwOYoTNc7gdO48Fwd0y3nCHXUkPvFe=w1236-h980
+
+//* images Henry Games
+// imgH https://lh3.googleusercontent.com/drive-viewer/AFGJ81ovsTz5CU2kPeuP49t47syGzuuXMlJtgKfnOJDtl9bjS4zmSdBpDJlhspCuh9MN1akqvV7BL37ZFZ70Rs7Hk6XQ79WjWw=w1236-h980
+// imgH1 https://lh3.googleusercontent.com/drive-viewer/AFGJ81qk0AHIfa9OhHT9GDG6M-zGZiedDr4WU8_chQ0sF2Qc6cR6nvJL-yxWtTHYzm7oInhV91CTo-jxibfux86y3DvF2qY8=w1920-h980
+// imgH2 https://lh3.googleusercontent.com/drive-viewer/AFGJ81oCAOsDSzhrcr8jbOX78_dQofEvtehx4sTO-4m-hbR1TDMiR4NtO5q0-AKJ3oT4gNG683p6PRFrrROOCBAk24A9ZEgonA=w1236-h980
+// imgH3 https://lh3.googleusercontent.com/drive-viewer/AFGJ81p3myNwxajyDppWk8Q692BhW4XO2dPuHs6ab53Tbu3uTSbt_yx3vhdey2enHA-C2_JshOPzf6_NeFwNiSiZfxJGPAtHEQ=w1236-h980
+// imgH4 https://lh3.googleusercontent.com/drive-viewer/AFGJ81p3OATKyx03jVh8LcscSgnJoGpk1LP6B1WT5KwwUD_QMOb4JRXmqZRueByx4x5rMNdlUVcbQeWqcC8byZO36o1iDfzh=w1236-h980
+// imgH5 https://lh3.googleusercontent.com/drive-viewer/AFGJ81qmdcqHmB45NN9Bb6PFTcYWabjAHJLC4qY4kNvPj_1bBK1_ALxHWVUNk74P9_B4bKq3PMVAnEStleV_HvQt2qTQFDT3=w1236-h980
+// imgH6 https://lh3.googleusercontent.com/drive-viewer/AFGJ81pGb8SVzGpWlAu0cwCGnkDE7sv5PK0tr3cmol9NZGVQce6BENtFwVBV6BfwnuPmgbZUQ5kUhA_DTTDnXjZeqlIFAVff=w1236-h980
+// imgH7 https://lh3.googleusercontent.com/drive-viewer/AFGJ81pqE2w95zzAtDJ-GgybsqwNT_c0VnSRSjwcSNHZ987M0bnSy77aM8pIz14I9b_s-ONqMILXyGSIsIGFF8ue0LfnMu9rBQ=w1236-h980
+// imgH8 https://lh3.googleusercontent.com/drive-viewer/AFGJ81qM-lXuj6HHHSUjUXmPZkUnDxNK-gjKBtXLNX8PHoq78l18cWl4twuEPwI0FHNyTa4jSEnFv9CDkpxins5xSEBfSg4szQ=w1236-h980
+// imgH9 https://lh3.googleusercontent.com/drive-viewer/AFGJ81qCl3WYJBwU3LDEg8HN1mSmr4Mmozlrjeabz8i4pUkHht8YCj4Ia31hgOMTMX0loDguNfneVzZ13SeVQAS0KO-UQmZSmg=w1236-h980
+// imgH10 https://lh3.googleusercontent.com/drive-viewer/AFGJ81pQ1nl9NaCvcbn5vsXP8-GNyLnJxh0qLYWSJhj8TsSWNMnTFt_pH8AUXzAVtpoeAfpPkXflaZcbapGQ2HG_D0qae34XJg=w1236-h980
+// imgH11 https://lh3.googleusercontent.com/drive-viewer/AFGJ81rxrByplALpzt36lbKUgNlojl143rDLoq_6A6MOJ6Eo6-iNXo9u_xQtQNuo1hiTxDNOIQFL6ZKEYYsBs1NkgtuqfccNyA=w1236-h980
+
+// icons
 import html from '../images/technologies/html.png'
 import java from '../images/technologies/java.png'
 import js from '../images/technologies/js.png'
@@ -13,7 +54,6 @@ import scrum from '../images/technologies/SCRUM.png'
 
 export const admin = {
   id: 1,
-  img: { perfil },
   name: 'William',
   last_name: 'henao',
   mail: 'alexanderhenao0522@gmail.com',
@@ -41,35 +81,23 @@ export const Proyectos = [
     description:
       'Henry Games es una plataforma que permite a los usuarios gestionar información de videojuegos de manera intuitiva y eficiente, también integra una API que proporciona información detallada sobre más de 300.000 videojuegos, lo que hace que la gestión de la información sea aún más completa y actualizada.',
     skills: [skills[1], skills[2], skills[3], skills[4], skills[5], skills[6], skills[7]],
-    images: [
-      {
-        id: 1,
-        img: {}
-      }
-    ]
+    // images: [imgH, imgH1, imgH2, imgH3, imgH4, imgH5, imgH6, imgH7, imgH8, imgH9, imgH10, imgH11],
+    state: 'complete'
   },
   {
     id: 2,
     name: 'FusionaJob',
     description: 'Una app que permite conectar a candidatos y reclutadores',
-    skills: [{ id: 1, name: 'HTML', icon: {} }],
-    images: [
-      {
-        id: 1,
-        img: {}
-      }
-    ]
+    skills: [skills[1], skills[2], skills[8], skills[4], skills[5], skills[6], skills[7]],
+    // images: [imgF, imgF2, imgF2b, imgF3, imgF4, imgF5, imgF6, imgF7, imgF8, imgF9, imgF10, imgF11, imgF12, imgF13, imgF14, imgF15, imgF16, imgF17, imgF18, imgF19, imgF20, imgF21, imgF22, imgF23, imgF24],
+    state: 'complete'
   },
   {
     id: 3,
     name: 'Portafolio',
     description: 'Es donde estáis',
-    skills: [{ id: 1, name: 'HTML', icon: {} }],
-    images: [
-      {
-        id: 1,
-        img: {}
-      }
-    ]
+    skills: [skills[1], skills[2], skills[3], skills[11], skills[10], skills[8], skills[7]],
+    images: [],
+    state: 'in_progress'
   }
 ]
