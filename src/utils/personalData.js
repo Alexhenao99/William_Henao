3977b73dab7ce4459171304765b018cb84b2a76e
Alex Henao <alexanderhenao0522@gmail.com>
2023-06-30 11:59:51 -0500

    Update Cv

--- src/utils/personalData.js
+++ src/utils/personalData.js
@@ -1,6 +1,6 @@
 const data = {
   correo: 'waha0522@gmail.com',
-  cvUrl: 'https://drive.google.com/file/d/1HA-bhLAPOGcjuDaeiojqQzOBn4Gt3COL/view?usp=share_link'
+  cvUrl: 'https://drive.google.com/file/d/1NXn423L-MO_PS-kCWpUpHHtP6VJnUaPj/view?usp=share_link'
 }
 
 export const mail = () => {
